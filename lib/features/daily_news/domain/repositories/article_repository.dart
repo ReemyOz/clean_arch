70cee7e23f55bfc0ff016a76c2cc89c18f0e64af
Reem Onez <reem66onez@gmail.com>
2024-08-29 15:44:19 +0300

    initial structure of the project

---
+++ lib/features/daily_news/domain/repositories/article_repository.dart
@@ -0,0 +1,14 @@
+import 'package:fetching_data/core/resources/data_state.dart';
+import 'package:fetching_data/features/daily_news/domain/entities/article.dart';
+
+abstract class ArticleRepository {
+  // API methods
+  Future<DataState<List<ArticleEntity>>> getNewsArticles();
+
+  // Database methods
+  Future<List<ArticleEntity>> getSavedArticles();
+
+  Future<void> saveArticle(ArticleEntity article);
+
+  Future<void> removeArticle(ArticleEntity article);
+}
